b6d9957d3fc88a064f1b7e2e65f722c2b4089de9
mbp1225 <matheus698@hotmail.com>
2018-08-06 17:32:37 -0300

    System Start

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+	[SerializeField] Transform[] items;
+	[SerializeField] Transform indicator;
+	int currentItem = 0;
 
 	void Start ()
 	{
-		
+		StartCoroutine(ChangeItem());
 	}
 	
-	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (Input.GetKeyDown(KeyCode.UpArrow)) Navigate("up");
+		if (Input.GetKeyDown(KeyCode.UpArrow)) Navigate("down");
+	}
+
+	public void Navigate (string dir)
+	{
+		if (dir == "up")
+		{
+			if (currentItem == 0) currentItem = items.Length - 1;
+			else currentItem--;
+			StartCoroutine(ChangeItem());
+		}
+		if (dir == "down")
+		{
+			if (currentItem == items.Length - 1) currentItem = 0;
+			else currentItem++;
+			StartCoroutine(ChangeItem());
+		}
+	}
+
+	public void Confirm()
+	{
+
+	}
+
+	IEnumerator ChangeItem()
+	{
+		yield return new WaitForSeconds(0.001f);
+		indicator.position = new Vector3(indicator.position.x, items[currentItem].position.y);
 	}
 }
