8b31c77d43510c0e0a726b0b23afa1430d25f3c0
mbp1225 <matheus698@hotmail.com>
2018-08-08 01:16:55 -0300

    Menu System Start

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class ButtonManager : MonoBehaviour, ISelectHandler
+{
+    [SerializeField] private AudioClip buttonSound;
+    private AudioSource aSource;
+
+    private void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        aSource.PlayOneShot(buttonSound);
+    }
+
+}
---
+++ Assets/Scripts/UI/SFXManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+	private AudioSource source;
+
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
+	}
+
+	public void PlaySound()
+	{
+		source.PlayOneShot(source.clip,1f);
+	}
+	
+}
