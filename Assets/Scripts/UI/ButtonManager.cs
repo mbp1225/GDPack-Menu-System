58cba503800e35d689ebf1e674c01388070dd24a
mbp1225 <matheus698@hotmail.com>
2018-08-08 12:56:41 -0300

    Tutorial Screen Implemented

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -6,8 +6,19 @@ using UnityEngine.EventSystems;// Required when using Event data.
 
 public class ButtonManager : MonoBehaviour, ISelectHandler
 {
+    [Header("References")]
     [SerializeField] private AudioClip buttonSound;
     private AudioSource aSource;
+
+    [SerializeField] TutorialScreen tutorial;
+
+    [SerializeField] Color[] colors;
+    [SerializeField] RawImage indicator;
+
+    [Header("Tutorial Info")]
+    [SerializeField] string gameName;
+    [SerializeField] string tutorialText;
+    [SerializeField] Texture tutorialImage;
 
     private void Start()
     {
@@ -16,7 +27,19 @@ public class ButtonManager : MonoBehaviour, ISelectHandler
 
     public void OnSelect(BaseEventData eventData)
     {
+        indicator.color = colors[Random.Range(0,3)];
         aSource.PlayOneShot(buttonSound);
     }
 
+    public void Press()
+    {
+        SetTutorial();
+        print("Pressed " + gameObject.name);
+    }
+
+    void SetTutorial()
+    {
+        tutorial.gameObject.SetActive(true);
+        tutorial.Tutorial(gameName, tutorialText, tutorialImage);
+    }
 }
---
+++ Assets/Scripts/UI/TutorialScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TutorialScreen : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI title;
+	[SerializeField] TextMeshProUGUI text;
+	[SerializeField] RawImage image;
+
+	public void Tutorial(string gameTitle, string tutorialText, Texture gameImage)
+	{
+		title.text = gameTitle;
+		text.text = tutorialText;
+		image.texture = gameImage;
+	}
+}
